f699e15b6ea63ea4783b94f889e9505f1fa9c111
AYDOSUL <sural.adyoth@gmail.com>
2025-03-30 18:35:39 -0400

    Created gradient charge computation

--- ADAPT_VQE_H3.py
+++ ADAPT_VQE_H3.py
@@ -1,12 +1,11 @@
-#ADAPT-VQE on H3
+#ADAPT-VQE on H2
 import pennylane as qml
 from pennylane import numpy as np
 
 symbols = ["H", "H", "H"]
-geometry = np.array([[0.01076341, 0.04449877, 0.0],
-                     [0.98729513, 1.63059094, 0.0],
-                     [1.87262415, -0.00815842, 0.0]], requires_grad=False)
-H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge = 1)
+geometry = coordinates = np.array([[0.0102,0.0442,0.0],[0.9867,1.6303,0.0],
+                        [1.8720,-0.0085,0.0]], )
+H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=1)
 
 n_electrons = 2
 singles, doubles = qml.qchem.excitations(n_electrons, qubits)
@@ -20,13 +19,17 @@ dev = qml.device("default.qubit", wires=qubits)
 def circuit():
     qml.BasisState(hf_state, wires=range(qubits))
     return qml.expval(H)
-
+prev_gradient = 1
+gradient = 1
 opt = qml.AdaptiveOptimizer()
 for i in range(len(operator_pool)):
+    prev_gradient = gradient
     circuit, energy, gradient = opt.step_and_cost(circuit, operator_pool, drain_pool=True)
+    derivative_gradient = gradient/prev_gradient
     print('Energy:', energy)
     print(qml.draw(circuit, show_matrices=False)())
     print('Largest Gradient:', gradient)
+    print('Gradient Prime:', derivative_gradient)
     print()
-    if gradient < 1e-8:
+    if gradient < 1e-10:
         break
